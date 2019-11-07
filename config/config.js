0ac7577a9cee59ddbbd169caa55596ca3eaaa825
zhangxiang <betterzx153@gmail.com>
2019-11-07 16:06:56 +0800

    save

--- config/config.js
+++ config/config.js
@@ -5,6 +5,9 @@ import webpackPlugin from './plugin.config';
 const { pwa, primaryColor } = defaultSettings; // preview.pro.ant.design only do not use in your production ;
 // preview.pro.ant.design 专用环境变量，请不要在你的项目中使用它。
 
+// "hooks": {
+//   "pre-commit": "npm run lint-staged"
+// }
 const { ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION } = process.env;
 const isAntDesignProPreview = ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION === 'site';
 
